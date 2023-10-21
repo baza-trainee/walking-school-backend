065ee6ca6ecdeab46616939c8d104d3c39401642
Yehor Tverytinov <etverya11@gmail.com>
2023-10-21 12:12:38 +0300

    first deploy 2

--- docs/docs.go
+++ docs/docs.go
@@ -2130,7 +2130,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:7000",
+	Host:             "https://walking-school.site",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Walking-School backend API",
